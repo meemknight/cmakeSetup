8e393399f215159e26526e1a351e1b2b16ae3a2a
vlod <vladalex420@gmail.com>
2021-02-19 14:01:45 +0200

    uodated readme

--- sources/main.cpp
+++ sources/main.cpp
@@ -46,7 +46,7 @@ int main()
 	renderer.create();
 
 	
-	gl2d::Font f(RESOURCES_PATH "roboto_black.ttf");
+	gl2d::Font font(RESOURCES_PATH "roboto_black.ttf");
 	gl2d::Texture texture(RESOURCES_PATH "test.jpg");
 
 	long lastTime = clock();
@@ -70,6 +70,8 @@ int main()
 		renderer.renderRectangle({ 100,350, 100, 100 }, { 0,0 }, 0, texture);
 		glm::vec4 colors[4] = { Colors_Orange,Colors_Orange ,Colors_Orange ,Colors_Orange };
 		renderer.renderRectangle({ 10,10, 100, 100 }, colors, {}, 30);
+
+		renderer.renderText({ 100,100 }, "Hello", font, Colors_White);
 
 		renderer.flush();
 		glfwSwapBuffers(wind);
