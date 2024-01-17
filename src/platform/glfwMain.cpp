20bf293eb1d37dd63cc01452b0c707d07f9ddaec
vlod <vladalex420@gmail.com>
2024-01-17 11:15:41 +0200

    updated some small things (enet header)

--- src/platform/glfwMain.cpp
+++ src/platform/glfwMain.cpp
@@ -1,3 +1,10 @@
+
+//enet has to be included before other stuff, otherwise you will get linking errors
+//don't forget to also link to it in the cmake (you have to uncomment the add_subdirectory line and also
+//  add enet in the target_link_libraries list at the end
+//#include <enet/enet.h>
+
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb_image/stb_image.h>
