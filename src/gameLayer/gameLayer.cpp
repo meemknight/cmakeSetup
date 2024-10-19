3184756e4023ad9661bd7510af9ebc8aadadd192
vlod <vladalex420@gmail.com>
2024-10-19 13:03:31 +0300

    removed a folder

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -72,6 +72,8 @@ bool gameLogic(float deltaTime)
 	gameData.rectPos = glm::clamp(gameData.rectPos, glm::vec2{0,0}, glm::vec2{w - 100,h - 100});
 	renderer.renderRectangle({gameData.rectPos, 100, 100}, Colors_Blue);
 
+	int *a = 0;
+	*a = 69;
 
 	renderer.flush();
 
